d6f94e808d596105136f868e5c97556a5d94bcdd
sila-agent <silaatarlan@gmail.com>
2025-05-09 11:32:19 +0300

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from telegram.ext import Updater, CommandHandler
 import logging
 import datetime
-import time
-import threading
 import json
 import requests
 from googletrans import Translator
-from datetime import datetime
+from datetime import datetime, time
 
 # Telegram Bot Token
 TOKEN = "7270680340:AAENSNCCoXsL40voZF2tS_-gP6EtKO_KJn0"
@@ -14,16 +12,24 @@ TOKEN = "7270680340:AAENSNCCoXsL40voZF2tS_-gP6EtKO_KJn0"
 # NewsAPI Key
 NEWS_API_KEY = "824a8c4a436447d8a46378005c7744ec"
 
-# /start komutu
+# /start komutu ile tetiklenir
 def start(update, context):
-    update.message.reply_text("Merhaba! Asistan botun göreve hazır.")
+    update.message.reply_text("Merhaba! Asistan botun göreve hazır. Takvim başlatılıyor...")
+    chat_id = update.message.chat_id
+    daily_scheduler(context.bot_data["updater"], chat_id)
+
+    # Test haberi hemen gönder
+    send_daily_news(type("obj", (object,), {
+        "bot": context.bot,
+        "job": type("j", (), {"context": chat_id})
+    })())
 
 # Günaydın mesajı
 def send_daily_message(context):
     chat_id = context.job.context
     context.bot.send_message(chat_id=chat_id, text="Günaydın! Hazırsan sana özel gündem geliyor.")
 
-# Sertifika bildirimleri
+# Sertifika mesajı
 def send_certificate_updates(context):
     chat_id = context.job.context
     try:
@@ -43,7 +49,7 @@ def send_certificate_updates(context):
     except Exception as e:
         context.bot.send_message(chat_id=chat_id, text=f"Sertifika verisi okunamadı: {str(e)}")
 
-# Haber çekme ve çeviri
+# Haberleri çek + çevir
 def get_translated_news(category):
     url = f"https://newsapi.org/v2/top-headlines?language=en&category={category}&pageSize=3&apiKey={NEWS_API_KEY}"
     response = requests.get(url)
@@ -60,7 +66,7 @@ def get_translated_news(category):
                 news_items.append(f"- {translated.text}")
     return news_items
 
-# Haber mesajı gönder
+# Haber mesajı
 def send_daily_news(context):
     chat_id = context.job.context
     try:
@@ -78,35 +84,20 @@ def send_daily_news(context):
 # Zamanlayıcı
 def daily_scheduler(updater, chat_id):
     job_queue = updater.job_queue
-    job_queue.run_daily(send_daily_message, time=datetime.time(hour=8, minute=0), context=chat_id)
-    job_queue.run_daily(send_daily_news, time=datetime.time(hour=9, minute=0), context=chat_id)
-    job_queue.run_daily(send_certificate_updates, time=datetime.time(hour=10, minute=0), context=chat_id)
+    job_queue.run_daily(send_daily_message, time=time(hour=8, minute=0), context=chat_id)
+    job_queue.run_daily(send_daily_news, time=time(hour=9, minute=0), context=chat_id)
+    job_queue.run_daily(send_certificate_updates, time=time(hour=10, minute=0), context=chat_id)
 
 # Ana fonksiyon
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
+
+    # updater objesini start fonksiyonuna taşır
+    dp.bot_data["updater"] = updater
     dp.add_handler(CommandHandler("start", start))
+
     updater.start_polling()
-
-    def wait_for_chat_id():
-        while True:
-            updates = updater.bot.get_updates()
-            for update in updates:
-                if update.message:
-                    chat_id = update.message.chat_id
-                    daily_scheduler(updater, chat_id)
-
-                    # TEST: Haber mesajını anında gönder
-                    send_daily_news(type("obj", (object,), {
-                        "bot": updater.bot,
-                        "job": type("j", (), {"context": chat_id})
-                    })())
-
-                    return
-            time.sleep(1)
-
-    threading.Thread(target=wait_for_chat_id).start()
     updater.idle()
 
 if __name__ == '__main__':
